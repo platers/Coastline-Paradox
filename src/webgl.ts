eb652327c63926fb17f7d1720cf70e551ab57a5b
platers <platers81@gmail.com>
2021-10-22 00:07:59 -0400

    refactor renderloop

--- src/webgl.ts
+++ src/webgl.ts
@@ -9,12 +9,54 @@ type Chunks = {
 
 export async function main(chunks: Chunks) {
   // Get A WebGL context
-  var canvas = document.querySelector("#canvas") as HTMLCanvasElement;
-  var gl = canvas.getContext("webgl");
+  const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
+  const gl = canvas.getContext("webgl");
   if (!gl) {
     return;
   }
 
+  var { resolutionUniformLocation, offset } = setupGl(gl);
+
+
+  // Render loop
+  function render(timestamp: number) {
+    // Clear the canvas
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // set the resolution
+    gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+
+    // Get lines
+    const lines = getLines(chunks);
+
+    // flatten the lines into a single array
+    const normalized_lines = lines.flat().map(point => [(1 - (point[0] + 180) / 360) * gl.canvas.width, (1 - (point[1] + 90) / 180) * gl.canvas.height]);
+
+    setLine(gl, normalized_lines.flat());
+    drawLines(gl, lines.length);
+
+    window.requestAnimationFrame(render);
+  }
+  window.requestAnimationFrame(render);
+}
+
+function drawLines(gl: WebGLRenderingContext, count: number) {
+  var primitiveType = gl.LINES;
+  var offset = 0;
+  gl.drawArrays(primitiveType, offset, count * 2);
+}
+
+function getLines(chunks: Chunks) {
+  let lines: Line[] = [];
+  // flatten the lines into a single array
+  for (const [chunk, lines1] of Object.entries(chunks)) {
+    lines = lines.concat(lines1);
+  }
+  return lines;
+}
+
+function setupGl(gl: WebGLRenderingContext) {
   // Get the strings for our GLSL shaders
   var vertexShaderSource = (document.querySelector("#vertex-shader-2d") as HTMLScriptElement).text;
   var fragmentShaderSource = (document.querySelector("#fragment-shader-2d") as HTMLScriptElement).text;
@@ -53,27 +95,14 @@ export async function main(chunks: Chunks) {
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
   // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-  var size = 2;          // 2 components per iteration
-  var type = gl.FLOAT;   // the data is 32bit floats
+  var size = 2; // 2 components per iteration
+  var type = gl.FLOAT; // the data is 32bit floats
   var normalize = false; // don't normalize the data
-  var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
-  var offset = 0;        // start at the beginning of the buffer
+  var stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
+  var offset = 0; // start at the beginning of the buffer
   gl.vertexAttribPointer(
-      positionAttributeLocation, size, type, normalize, stride, offset);
-
-  // set the resolution
-  gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
-
-  for (const [chunk, lines] of Object.entries(chunks)) {
-    // flatten the lines into a single array
-    const normalized_lines = lines.flat().map(point => [(1 - (point[0] + 180) / 360) * gl.canvas.width, (1 - (point[1] + 90) / 180) * gl.canvas.height]);
-    setLine(gl, normalized_lines.flat());
-    // Draw the loop.
-    var primitiveType = gl.LINES;
-    var offset = 0;
-    var count = lines.length * 2;
-    gl.drawArrays(primitiveType, offset, count);
-  }
+    positionAttributeLocation, size, type, normalize, stride, offset);
+  return { resolutionUniformLocation, offset };
 }
 
 function setLine(gl, line) {
